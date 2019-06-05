5033b49de41ce33e69e7267114dc97b972585246
Cristian Martin Sguazzin <34490158+NeXuZZ-SCM@users.noreply.github.com>
2019-06-05 19:46:06 -0300

    Add files via upload

---
+++ ClienteWCFFullDuplex/ClienteWCFFullDuplex/LlamadaCallBack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel;
+using ClienteWCFFullDuplex.ServiceReference1;
+using System.Threading;
+
+namespace ClienteWCFFullDuplex
+{
+    public class LlamadaACallBack : IService1Callback
+    {
+        public void EnviarMensaje()
+        {
+            var instanceContext = new InstanceContext(this);
+            var service = new Service1Client(instanceContext);
+            service.Conectar("consola");
+            Console.Read();
+
+        }
+
+        public void Mostrar(int numero)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
---
+++ ClienteWCFFullDuplex/ClienteWCFFullDuplex/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ClienteWCFFullDuplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LlamadaACallBack llamadaACallBack = new LlamadaACallBack();
+            llamadaACallBack.EnviarMensaje();
+        }
+    }
+    
+}
